ea3a7c777cab84edeea22d4c3e4a0bb48691e452
vinayakcc75 <46587781+vinayakcc75@users.noreply.github.com>
2019-08-11 14:34:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var name="Vinayak";
+var pass="1234";
+alert("This site belongs to 'Vinayak' Sign in to move further");
+var fname =prompt("Enter Username :");
+var fpass=prompt("Enter Password :");
+if(fname===name&&fpass===pass)
+{
+	alert("OK");
+}
+else
+{
+	alert("Enjoy!!!");
+}
