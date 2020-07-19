a70aa48d5bf05ca2c12a4eebed76e1dd55f33e26
Vinayak Chaudhary <vinayak@Vinayaks-MacBook-Air.local>
2020-07-19 19:34:01 +0530

    done

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var name="Vinayak";
-var pass="1234";
-alert("This site belongs to 'Vinayak' Sign in to move further");
-var fname =prompt("Enter Username :");
-var fpass=prompt("Enter Password :");
-if(fname===name&&fpass===pass)
-{
-	alert("OK");
-}
-else
-{
-	alert("Enjoy!!!");
-}
+// var name="Vinayak";
+// var pass="1234";
+// alert("This site belongs to 'Vinayak' Sign in to move further");
+// var fname =prompt("Enter Username :");
+// var fpass=prompt("Enter Password :");
+// if(fname===name&&fpass===pass)
+// {
+// 	alert("OK");
+// }
+// else
+// {
+// 	alert("Enjoy!!!");
+// }
